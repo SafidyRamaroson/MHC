0f697c5152bcf6089b9d60c73cecb93e238ccd9a
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-04-30 19:51:51 +0300

    Replace logo image with text and icon representation in Logo component

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { Quote } from "lucide-react";
 import Image from "next/image";
-import LogoImg from "@/public/assets/logo.png";
 
 
 export function Logo(){
     return(
-        <Image src={LogoImg} alt="Logo Image" width={40} height={30} className="rounded-full"/>
+        <span><Quote />Serenity Haven <br /> Wellness </span>
     )
 }
